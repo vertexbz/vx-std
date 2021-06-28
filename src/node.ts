cf67e0ce066c882aa4807e2a259869d3a8bde90b
vertex <adam.makswiej@gmail.com>
2021-06-28 21:27:16 +0200

    fork

--- src/node.ts
+++ src/node.ts
@@ -1,17 +1,80 @@
 /* eslint-disable @typescript-eslint/ban-types */
+import { fork as nodeFork } from 'child_process';
 import RemoteControlledPromise from './classes/RemoteControlledPromise';
 import * as promise from './promise';
 import * as predicate from './predicate';
 
+import type { ChildProcess, ForkOptions as NodeForkOptions } from 'child_process';
+
+export interface ForkOptions extends NodeForkOptions {
+    restartDelay?: number;
+    restart?: boolean;
+}
+
+export interface ForkMeta {
+    process: ChildProcess|null;
+    promise: Promise<undefined>;
+    terminate(): void;
+}
+
+export function fork(module: string, payload: Record<string, any>, forkOptions?: ForkOptions): ForkMeta {
+    const options: ForkOptions = {
+        restartDelay: 5000,
+        restart: false,
+        detached: false,
+        ...forkOptions
+    };
+
+    const promise = new RemoteControlledPromise<undefined>();
+
+    let running = true;
+
+    const meta: ForkMeta = {
+        promise: promise.promise,
+        process: null,
+        terminate() {
+            running = false;
+            meta.process?.kill();
+        }
+    };
+
+    const runProcess = () => {
+        const process = nodeFork(module, [JSON.stringify(payload)], options);
+        meta.process = process;
+
+        process.on('close', (code: number) => {
+            meta.process = null;
+            if (options.restart && running) {
+                setTimeout(runProcess, options.restartDelay);
+            } else if (code === 0) {
+                promise.resolve();
+            } else {
+                promise.reject(code);
+            }
+        });
+    };
+
+    runProcess();
+    return meta;
+}
+
+export function paramsFromJsonPayload<P = Record<string, any>>(offset = 2, base?: P): () => Promise<P> | P {
+    return function (): Promise<P> | P {
+        return Object.assign({}, base || {}, JSON.parse(process.argv[offset]));
+    };
+}
+
 export type DisposerSig = () => void
-export type InitRoutineSig = () => Promise<object | void> | object | void
-export type ServiceRoutineSig = (params: object) => Promise<void> | void;
 export type ExitSig = (sig: NodeJS.Signals) => Promise<void> | void;
 
-export interface ServiceParamsBase {
+export interface ServiceParams {
     quit(): void;
     onExit(cb: ExitSig): DisposerSig;
+    params: Record<string, any>
 }
+
+export type InitRoutineSig = () => Promise<object | void> | object | void
+export type ServiceRoutineSig = (serviceParams: ServiceParams) => Promise<void> | void;
 
 export type ServiceConfig = {
     keepAlive?: boolean,
@@ -70,25 +133,26 @@ export function service(...routines: any[]) {
         const loopParams = inits.filter(predicate.isPlainObject)
             .reduce((acc, obj) => {
                 return { ...obj, ...acc };
-            }, {
-                quit(reason?: any) {
-                    if (reason) {
-                        keepAlive.reject(reason);
-                    } else {
-                        keepAlive.resolve();
-                    }
-                },
-                onExit(cb: ExitSig) {
-                    exitCallbacks.add(cb);
-                    return () => {
-                        exitCallbacks.delete(cb);
-                    };
-                }
-            } as ServiceParamsBase);
+            }, {});
 
         while (keepAlive.running) {
             try {
-                await loopRoutine(loopParams);
+                await loopRoutine({
+                    quit(reason?: any) {
+                        if (reason) {
+                            keepAlive.reject(reason);
+                        } else {
+                            keepAlive.resolve();
+                        }
+                    },
+                    onExit(cb: ExitSig) {
+                        exitCallbacks.add(cb);
+                        return () => {
+                            exitCallbacks.delete(cb);
+                        };
+                    },
+                    params: loopParams
+                });
                 if (options.keepAlive) {
                     await keepAlive.promise;
                 }
